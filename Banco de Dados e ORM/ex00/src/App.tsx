c690cdc9a3169fd07610bd689454c9d93f722248
Gabriel Teixeira <139800510+joaogabrielts@users.noreply.github.com>
2025-02-10 12:12:40 -0300

    banco de dados

--- Banco de Dados e ORM/ex00/src/App.tsx
+++ Banco de Dados e ORM/ex00/src/App.tsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <h1>3 Componentes de um Banco de Dados</h1>
+      <p>Um banco de dados é composto por três componentes principais:</p>
+      <ol>
+        <li>
+          <p>
+            Dados: Os próprios dados que estão sendo armazenados. Esses dados
+            podem representar informações sobre pessoas, produtos, transações,
+            eventos, e muito mais, dependendo do contexto do banco de dados.
+          </p>
+        </li>
+        <li>
+          <p>
+            Software de Gerenciamento de Banco de Dados (SGBD): O SGBD é um
+            sistema de software responsável por gerenciar o armazenamento, a
+            organização e o acesso aos dados. Ele fornece uma interface para os
+            usuários e aplicativos interagirem com o banco de dados.
+          </p>
+        </li>
+        <li>
+          <p>
+            Hardware: O hardware inclui os servidores e dispositivos de
+            armazenamento que mantêm os dados fisicamente. A capacidade e o
+            desempenho do hardware têm um impacto significativo no desempenho do
+            banco de dados.
+          </p>
+        </li>
+      </ol>
+      <h1>4 Importância dos Bancos de Dados</h1>
+      <p>
+        Os bancos de dados desempenham um papel crucial em muitos aspectos da
+        vida moderna, incluindo:
       </p>
-    </>
-  )
+      <ul>
+        <li>
+          <p>
+            Armazenamento Eficiente: Eles permitem o armazenamento eficiente e
+            estruturado de grandes volumes de dados.
+          </p>
+        </li>
+        <li>
+          <p>
+            Recuperação Rápida: Facilitam a recuperação rápida e precisa de
+            informações específicas.
+          </p>
+        </li>
+        <li>
+          <p>
+            Consistência de Dados: Garantem a integridade e consistência dos
+            dados, prevenindo duplicatas e erros.
+          </p>
+        </li>
+        <li>
+          <p>
+            Suporte a Aplicativos: São essenciais para aplicativos de software
+            que precisam armazenar e recuperar dados, como aplicativos de
+            e-commerce, redes sociais e sistemas de gerenciamento de estoque.
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ Banco de Dados e ORM/ex01/src/App.tsx
@@ -0,0 +1,75 @@
+function App() {
+  return (
+    <div>
+      <h1>1 Fundamentos da Importância dos Bancos de Dados</h1>
+      <ol>
+        <li>
+          <p>
+            Armazenamento Eficiente: Os bancos de dados permitem o armazenamento
+            organizado e eficiente de grandes volumes de dados. Em um mundo onde
+            a quantidade de dados gerados diariamente está em constante
+            crescimento, essa eficiência é essencial.
+          </p>
+        </li>
+        <li>
+          <p>
+            Recuperação Rápida: Os bancos de dados são projetados para permitir
+            a recuperação rápida e precisa de informações específicas. Isso é
+            vital para sistemas que precisam fornecer respostas instantâneas,
+            como motores de busca na web.
+          </p>
+        </li>
+        <li>
+          <p>
+            Consistência de Dados: Os bancos de dados garantem a integridade e
+            consistência dos dados. Isso significa que os dados armazenados são
+            precisos, confiáveis e estão livres de duplicatas ou erros. Isso é
+            fundamental para tomadas de decisão baseadas em dados.
+          </p>
+        </li>
+      </ol>
+      <h1> Aplicações na Vida Moderna</h1>
+      <ol>
+        <li>
+          <p>
+            Comércio Eletrônico: Bancos de dados são usados para armazenar
+            informações sobre produtos, clientes, transações e histórico de
+            compras em lojas online. Isso permite recomendações personalizadas,
+            gestão de inventário eficiente e processamento de pedidos.
+          </p>
+        </li>
+        <li>
+          <p>
+            Redes Sociais: Plataformas de redes sociais como Facebook, Twitter e
+            Instagram dependem fortemente de bancos de dados para armazenar
+            perfis de usuários, postagens, conexões entre usuários e dados de
+            engajamento.
+          </p>
+        </li>
+        <li>
+          <p>
+            Saúde: Em hospitais e clínicas, bancos de dados armazenam registros
+            médicos eletrônicos, históricos de pacientes, informações de seguros
+            e dados de pesquisas clínicas.
+          </p>
+        </li>
+        <li>
+          <p>
+            Finanças: Bancos, instituições financeiras e empresas de fintech
+            utilizam bancos de dados para rastrear transações, históricos de
+            contas, relatórios de crédito e gerenciamento de riscos.
+          </p>
+        </li>
+        <li>
+          <p>
+            Análise de Dados: Empresas e pesquisadores usam bancos de dados para
+            armazenar grandes conjuntos de dados e realizar análises avançadas,
+            como aprendizado de máquina e mineração de dados.
+          </p>
+        </li>
+      </ol>
+    </div>
+  );
+}
+
+export default App;
