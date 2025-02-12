1dad4b3cf4570b90e63f017a96d4f9502800c6fb
Gabriel Teixeira <139800510+joaogabrielts@users.noreply.github.com>
2025-02-12 20:30:23 -0300

    lendo dados em sql

--- Banco de Dados e ORM/ex12-exemplo/src/App.tsx
+++ Banco de Dados e ORM/ex12-exemplo/src/App.tsx
@@ -5,60 +5,64 @@ function App() {
         <li>
           <h1>Introdução</h1>
           <p>
-            Nesta aula, vamos abordar a criação e exclusão de tabelas em bancos
-            de dados.
+            Nesta aula, exploraremos a inserção de dados em tabelas do
+            PostgreSQL.
           </p>
         </li>
         <li>
-          <h1>Criando tabelas</h1>
+          <h1> Inserindo recursos</h1>
           <p>
-            Para criar tabelas, utilizaremos o comando CREATE TABLE. Abaixo,
-            apresentamos um exemplo de código SQL:
+            Para inserir dados em tabelas, utilizaremos o comando INSERT INTO. O
+            formato padrão deste comando é ilustrado a seguir:
           </p>
-          <span>exemplo esta no codigo</span>
+          <h3>INSERT INTO (colunas) VALUES (valores);</h3>
+          <p>Vamos examinar um exemplo prático:</p>
+          <h3>
+            INSERT INTO products ("name", "description", "price", "createdAt",
+            "updatedAt") VALUES ('Computador', 'Computador bonito', 250000,
+            '2023-10-16', '2023-10-16');
+          </h3>
           <p>
-            Durante o comando de criação, você deve especificar o nome da tabela
-            e, ao abrir os parênteses, descrever cada uma das colunas da tabela.
-            Vamos conferir um exemplo prático:
+            Neste exemplo, inserimos um novo produto na tabela "products," e
+            observe que não foi necessário fornecer um valor para a coluna "id,"
+            pois ela é autoincrementada pelo banco de dados.
           </p>
-          <p>
-          CREATE TABLE products (
-        "id" SERIAL PRIMARY KEY,
-        "name" VARCHAR(250) NOT NULL,
-        "description" TEXT,
-        "price" INTEGER NOT NULL,
-        "createdAt" TIMESTAMP NOT NULL,
-        "updatedAt" TIMESTAMP NOT NULL
-        );
-          </p>
-          <p>
-            No exemplo acima, estamos criando uma tabela chamada "products" com as colunas "id," "name," "description," "price," "createdAt," e "updatedAt."
-          </p>
+          <span>
+            Neste exemplo, inserimos um novo produto na tabela "products," e
+            observe que não foi necessário fornecer um valor para a coluna "id,"
+            pois ela é autoincrementada pelo banco de dados.
+          </span>
           <div>
-            <h2>2.1 SERIAL PRIMARY KEY
+            <h1>2.1 Inserção múltipla</h1>
+            <p>
+              O comando INSERT não se limita a inserir um recurso de cada vez;
+              ele permite a inserção de vários recursos de uma só vez. Veja o
+              exemplo de código a seguir:
+            </p>
+            <h2>
+              INSERT INTO products ("name", "description", "price", "createdAt",
+              "updatedAt") VALUES ('Notebook', 'Notebook bonito', 350000,
+              '2023-10-16', '2023-10-16'), ('Mouse', 'Mouse bonito', 25000,
+              '2023-10-16', '2023-10-16');
             </h2>
-            <p>Observe que a coluna "id" foi definida como SERIAL, o que fará com que ela seja autoincrementada pelo banco de dados. Além disso, a marca PRIMARY KEY define a coluna como chave primária dos registros na tabela, o que será fundamental ao trabalhar com relacionamentos entre tabelas.</p>
-            <h2>2.2 NOT NULL
-            </h2>
-            <p>Uma novidade introduzida neste exemplo é a instrução NOT NULL, que torna a coluna obrigatória. Sem ela, por padrão, o SQL cria colunas opcionais.</p>
-            <h2>2.3 Aspas duplas (")</h2>
-            <p>No exemplo acima, o nome de cada coluna foi envolto em aspas duplas. Isso é uma boa prática, pois, se as aspas duplas estiverem ausentes, o PostgreSQL não fará distinção entre letras maiúsculas e minúsculas.</p>
-            <h2>2.4 Excluindo tabelas
-            </h2>
-            <p>Caso seja necessário excluir uma tabela, seja por erro na criação ou para efetuar alterações na estrutura, você pode usar o comando DROP TABLE. Confira o exemplo abaixo:</p>
-            <h3>DROP TABLE products; </h3>
+          </div>
+          <div>
+            <h1>2.2 RETURNING</h1>
+            <p>
+              Ao adicionar um ou mais recursos, é possível recuperar os valores
+              inseridos usando a cláusula RETURNING. Confira o exemplo abaixo:
+            </p>
+            <h3>
+              INSERT INTO products ("name", "description", "price", "createdAt",
+              "updatedAt") VALUES ('Computador Gamer', 'Computador gamer
+              bonito', 550000, '2023-10-16', '2023-10-16') RETURNING *;
+            </h3>
           </div>
         </li>
       </ol>
     </div>
   );
 }
-/*
-CREATE TABLE nome_da_tabela (
-    /* Colunas */
-//  );
-/* 
 
-*/
 
 export default App;
