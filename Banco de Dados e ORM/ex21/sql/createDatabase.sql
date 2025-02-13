ec398b991a1b83eafc80cf4e42e2ab96be8d2b41
Gabriel Teixeira <139800510+joaogabrielts@users.noreply.github.com>
2025-02-13 18:40:16 -0300

    Atividade - Praticando Relacionamento N:N

---
+++ Banco de Dados e ORM/ex21/sql/createDatabase.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE m4_aprendendo_sql;
+
+/* \l - lista todos os bancos de dados    */
+/* \c nome do banco - seleciona um banco de dados     */
+/* DROP DATABASE - excluir um banco de dado e basta coloca o nome */
+
+
