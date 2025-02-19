936767379c30de5866939127645e7ae458dbebc7
Gabriel Teixeira <139800510+joaogabrielts@users.noreply.github.com>
2025-02-18 21:30:15 -0300

    Create Many

--- Banco de Dados e ORM/ex40/src/index.ts
+++ Banco de Dados e ORM/ex40/src/index.ts
@@ -3,7 +3,7 @@ import {StoreServices } from "./services/store.services"
 const storeServices = new StoreServices
 
 async function exampleController(){
-    const response = await storeServices.create("Alex Store")
+    const response = await storeServices.createMany(["gabriel", "joão Gabriel"])
 
     console.log(response);
     
--- Banco de Dados e ORM/ex40/src/services/store.services.ts
+++ Banco de Dados e ORM/ex40/src/services/store.services.ts
@@ -1,7 +1,13 @@
 import { prisma } from "../database/prisma";
 
 export class StoreServices {
- async create(storeName: string) {
+  async create(storeName: string) {
     return await prisma.store.create({ data: { name: storeName } });
+  }
+
+  async createMany(storeNames: string[]) {
+    // nesse string[] não vai ser sempre que vai ser assim depende muuito como vai esta a sua tabela
+    const stores = storeNames.map((storeName) => ({ name: storeName }));
+    return await prisma.store.createMany({ data: stores });
   }
 }
