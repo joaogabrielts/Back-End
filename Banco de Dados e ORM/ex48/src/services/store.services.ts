34aeab2613abe7de480c691ebec8a42760528c2d
Gabriel Teixeira <139800510+joaogabrielts@users.noreply.github.com>
2025-02-19 20:36:27 -0300

    Prisma: Paginação

---
+++ Banco de Dados e ORM/ex48/src/services/store.services.ts
@@ -0,0 +1,35 @@
+import { prisma } from "../database/prisma";
+
+export class StoreServices {
+  async create(storeName: string) {
+    return await prisma.store.create({ data: { name: storeName } });
+  }
+
+  async createMany(storeNames: string[]) {
+    // nesse string[] não vai ser sempre que vai ser assim depende muuito como vai esta a sua tabela
+    const stores = storeNames.map((storeName) => ({ name: storeName }));
+    return await prisma.store.createMany({ data: stores });
+  }
+
+  async findOne(storeId: number) {
+    return await prisma.store.findFirst({
+      where: { id: storeId },
+      include: { products: true },
+    });
+  }
+
+  async findMany(search?: string, take = 10, skip = 0) {
+    return await prisma.store.findMany({
+      where: search ? { name: { contains: search, mode: "insensitive" } ,} : {},
+      take,
+      skip: skip  // ship * take vai fazer ter pagina umm pagina 2 e assim pra frente
+    });
+  }
+}
+
+/* 
+async findMany(search?: string) {
+  return await prisma.store.findMany({ where: { name: { contains: search, mode: "insensitive" } },
+  });
+}
+*/
