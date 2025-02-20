f5a3a12c3932b2b9bfe806714cced5fd4d4d9aab
Gabriel Teixeira <139800510+joaogabrielts@users.noreply.github.com>
2025-02-20 16:14:20 -0300

    Atividade Ações referencias

---
+++ atividade-Banco de Dados e ORM/ex55/prisma/migrations/20250220190630_on_delete/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Question" ALTER COLUMN "studentId" DROP NOT NULL;
---
+++ atividade-Banco de Dados e ORM/ex55/src/services/studentInfo.services.ts
@@ -0,0 +1,19 @@
+import { TCreateStudentInfoBody } from "../interface/studentInfo.interface";
+import { TUpdateStudentInfoBody } from "../interface/studentInfo.interface";
+import { prisma } from "../database/prisma";
+
+export class StudentInfo {
+  async create(body: TCreateStudentInfoBody) {
+    return await prisma.studentInfo.create({ data: body });
+  }
+  async updateMany(studentId: number, data: TUpdateStudentInfoBody) {
+    return await prisma.studentInfo.updateMany({
+      where: { studentId: studentId },
+      data,
+    });
+  }
+
+  async deleteOne(studentId: number) {
+    return await prisma.studentInfo.delete({ where: { id: studentId } });
+  }
+}
