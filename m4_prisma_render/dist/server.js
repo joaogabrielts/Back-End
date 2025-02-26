69d176ff1a8e05c58148a06a1ee6fd38e9fbda55
Gabriel Teixeira <139800510+joaogabrielts@users.noreply.github.com>
2025-02-26 11:40:19 -0300

    remoção

--- m4_prisma_render/dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const PORT = process.env.PORT || 3008;
-app_1.app.listen(PORT, () => {
-    console.log(`API sucessfully started at port ${PORT}`);
-});
--- m4_prisma_render_aula_77/src/server.ts
+++
@@ -1,8 +0,0 @@
-import { app } from "./app";
-
-const PORT = process.env.PORT || 3008;
-
-app.listen(PORT, () => {
-    console.log(`API sucessfully started at port ${PORT}`);
-})
-
