c10d810b1ae3f836ecf23350f0f350891344ab37
Gabriel Teixeira <139800510+joaogabrielts@users.noreply.github.com>
2025-04-10 16:33:43 -0300

    .

---
+++ Back-End com Python e Django/ex84/accounts/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+
+class AddressSerializer(serializers.Serializer):
+    street = serializers.CharField()
+    house_number = serializers.CharField()
+    city = serializers.CharField()
+    zip_code = serializers.CharField()
+
+
+class AccountSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    email = serializers.EmailField()
+    age = serializers.IntegerField()
+    sex = serializers.CharField()
+    addresses = AddressSerializer(many=True)
---
+++ Back-End com Python e Django/ex85/accounts/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+
+class AccountSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    email = serializers.EmailField()
+    age = serializers.IntegerField()
+    sex = serializers.CharField()
